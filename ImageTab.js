78342e8e26974424a96ff0f6c9405c7cf53b1933
ByzantineFailure <byzanitnefailure@gmail.com>
2018-10-26 00:24:55 -0700

    Commit copy tabs functionality.  Does not work in Edge :(

--- ImageTab.js
+++ ImageTab.js
@@ -9,3 +9,28 @@ browser.contextMenus.create({
     }); 
   },
 });
+
+browser.browserAction.onClicked.addListener(tab => {
+  browser.windows.getCurrent({populate: true}, currentWindow => {
+    if (!currentWindow.tabs) {
+      return;
+    }
+    const urls = currentWindow.tabs
+      .map(tab => tab.url).join('\n');
+    
+    const copyTarget = document.createElement('textarea');
+    copyTarget.value = urls;
+    document.body.appendChild(copyTarget);
+
+    copyTarget.focus();
+    copyTarget.select();
+
+    try {
+      document.execCommand('copy', false, null);
+    } catch (err) {
+      alert(err);
+    }
+    
+    document.body.removeChild(copyTarget);
+  });
+});
